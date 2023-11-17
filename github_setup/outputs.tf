4437e1bf781ac0fd3515c30bce583e50d152f5b0
Yemi <yemisiomonijo20@yahoo.com>
2023-11-16 19:37:08 -0500

    final testing

--- github_setup/outputs.tf
+++ github_setup/outputs.tf
@@ -2,27 +2,27 @@
 ################################################################################
 # Github actions variables
 ################################################################################
-output "eks_s3_bucket_id" {
+output "eks_tfstate_bucket" {
   description = "Name of the bucket for S3 eks backend"
   value       = module.eks_backend.s3_bucket_id
 }
 
-output "eks_table_id" {
+output "eks_tfstate_db" {
   description = "Name of dynamodb table for eks remote state"
   value       = module.eks_backend.table_id
 }
 
-output "pipeline_s3_bucket_id" {
+output "cicd_tfstate_bucket" {
   description = "Name of the bucket for S3 pipeline backend"
   value       = module.pipeline_backend.s3_bucket_id
 }
 
-output "pipeline_table_id" {
+output "cicd_tfstate_db" {
   description = "Name of dynamodb table for pipeline remote state"
   value       = module.pipeline_backend.table_id
 }
 
-output "github_role" {
+output "aws_role" {
   description = "github actions role"
   value       = aws_iam_role.github_role.arn
 }
